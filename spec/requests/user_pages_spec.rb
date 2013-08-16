4d7e4a48a402005fc3b7c60a764b34da9474a825
Tommaso <raspaccio@gmail.com>
2013-08-16 16:25:18 +0200

    test the page after signup

--- spec/requests/user_pages_spec.rb
+++ spec/requests/user_pages_spec.rb
@@ -41,6 +41,7 @@ describe "UserPages" do
     end
 
     describe "with valid information" do
+
       before do
         fill_in "Name",         with: "Example User"
         fill_in "Email",        with: "user@example.com"
@@ -51,6 +52,15 @@ describe "UserPages" do
       it "should create a user" do
         expect { click_button submit }.to change(User, :count).by(1)
       end
+
+      describe "after saving the user" do
+        before { click_button submit }
+        let(:user) { User.find_by(email: 'user@example.com') }
+
+        it { should have_title(user.name) }
+        it { should have_selector('div.alert.alert-success', text: 'Welcome') }
+      end
+
     end
 
   end
