8a4489d028d8c4781b7a83a790b9cafe8ad7650e
Tommaso <raspaccio@gmail.com>
2013-08-28 17:14:56 +0200

    add user to microposts association

--- app/models/user.rb
+++ app/models/user.rb
@@ -9,6 +9,8 @@ class User < ActiveRecord::Base
 
   has_secure_password
 
+  has_many :microposts
+
   def User.new_remember_token
     SecureRandom.urlsafe_base64
   end
--- spec/models/micropost_spec.rb
+++ spec/models/micropost_spec.rb
@@ -4,13 +4,21 @@ describe Micropost do
   let(:user) { FactoryGirl.create(:user) }
 
   before do
-    # This code is not idiomatically correct.
-    @micropost = Micropost.new(content: "Lorem ipsum", user_id: user.id)
+    @micropost = user.microposts.build(content: "Lorem ipsum")
   end
 
   subject { @micropost }
 
   it { should respond_to(:content) }
   it { should respond_to(:user_id) }
+  it { should respond_to(:user) }
+  its(:user) { should eq user }
+
+  it { should be_valid }
+
+  describe "when user_id is not present" do
+    before { @micropost.user_id = nil }
+    it { should_not be_valid }
+  end
 
 end
